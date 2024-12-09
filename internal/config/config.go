5f10b87b7fca17473ddf5b635a93f75f6ad909c1
mashfeii-manjaro <evil-corporaty@yandex.ru>
2024-12-09 20:24:01 +0300

    feat: config entity, validation

---
+++ internal/config/config.go
@@ -0,0 +1,56 @@
+package config
+
+import (
+	"strconv"
+	"strings"
+
+	"github.com/es-debug/backend-academy-2024-go-template/internal/infrastructure/errors"
+)
+
+const (
+	XMax   = 1.77
+	XMin   = -1.77
+	YMax   = 1.0
+	YMin   = -1.0
+	XRange = XMax - XMin
+	YRange = YMax - YMin
+)
+
+type Config struct {
+	Width       int
+	Height      int
+	N           int
+	ItNum       int
+	Samples     int
+	Symmetry    int
+	Transitions []int
+}
+
+func NewConfig(settings []int, trans string) (*Config, error) {
+	for _, val := range settings {
+		if val < 0 {
+			return nil, errors.NewErrInvalidIntegerFlag()
+		}
+	}
+
+	transitions := make([]int, 0)
+
+	for _, val := range strings.Split(trans, ",") {
+		converted, err := strconv.Atoi(val)
+		if err != nil {
+			return nil, err
+		}
+
+		transitions = append(transitions, converted)
+	}
+
+	return &Config{
+		Width:       settings[0],
+		Height:      settings[1],
+		ItNum:       settings[2],
+		Samples:     settings[3],
+		N:           settings[4],
+		Symmetry:    settings[5],
+		Transitions: transitions,
+	}, nil
+}
