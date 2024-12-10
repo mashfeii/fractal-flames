3b858b5b93ad5f4329c8ccd7b74eb76f88b0409a
mashfeii-manjaro <evil-corporaty@yandex.ru>
2024-12-10 05:27:46 +0300

    chore: minor fixes

--- internal/domain/pixel.go
+++ internal/domain/pixel.go
@@ -5,7 +5,7 @@ import "math"
 type FractalPixel interface {
 	Hitted() bool
 	Hit()
-	SetColor(r, g, b uint8)
+	SetColor(r, g, b, a uint8)
 }
 
 type Pixel struct {
@@ -21,7 +21,7 @@ func (p Pixel) RGBA() (r, g, b, a uint32) {
 	g |= g << 8
 	b = uint32(p.B)
 	b |= b << 8
-	a = 255
+	a = uint32(255)
 	a |= a << 8
 
 	return
